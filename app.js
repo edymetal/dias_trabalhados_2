b974f6d445bf1c5bdcddf5c64f94ac7f7d0db20b
Edney Gemini <edneypugliese.dev@gmail.com>
2026-05-30 13:39:09 -0300

    ui: force visual containment of calendar and manual version update v1.0.37

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.36';
-const APP_BUILD_DATE = '2026-05-30 13:33:38';
+const APP_VERSION = '1.0.37';
+const APP_BUILD_DATE = '2026-05-30 13:39:09';
 
 
 
