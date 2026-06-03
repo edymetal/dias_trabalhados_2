75d2a0d12f37830e42d852200c8ee58b4758b37c
Edney Gemini <edneypugliese.dev@gmail.com>
2026-06-03 19:37:27 -0300

    chore: remove firebase-config.js ref and bump script version to v1.0.45

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.45';
-const APP_BUILD_DATE = '2026-06-03 19:33:03';
+const APP_VERSION = '1.0.46';
+const APP_BUILD_DATE = '2026-06-03 19:37:28';
 
 
 
