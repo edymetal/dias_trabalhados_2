ede287894ba6e36ea9b88b5223d8d3f90029c25d
Edney Gemini <edneypugliese.dev@gmail.com>
2026-06-04 17:31:46 -0300

    style: restore gradient and glow effect to login screen logo

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.68';
-const APP_BUILD_DATE = '2026-06-04 17:26:34';
+const APP_VERSION = '1.0.69';
+const APP_BUILD_DATE = '2026-06-04 17:31:46';
 
 
 
