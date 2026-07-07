3145a2fe11647e797ff2d4b7ba60d8bebce411a9
Edney Gemini <edneypugliese.dev@gmail.com>
2026-07-07 16:12:54 -0300

    Ajusta visual do switch automatico

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.83';
-const APP_BUILD_DATE = '2026-07-07 16:08:08';
+const APP_VERSION = '1.0.84';
+const APP_BUILD_DATE = '2026-07-07 16:12:55';
 
 
 
