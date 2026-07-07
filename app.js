b890c2026d93f05fcda34b01f188842c3cd160f8
Edney Gemini <edneypugliese.dev@gmail.com>
2026-07-07 15:46:02 -0300

    Adiciona icones para atalho mobile

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.80';
-const APP_BUILD_DATE = '2026-06-07 19:17:41';
+const APP_VERSION = '1.0.81';
+const APP_BUILD_DATE = '2026-07-07 15:46:03';
 
 
 
