42cb8b3415d7eb384426f69260318287090606c2
Edney Gemini <edneypugliese.dev@gmail.com>
2026-07-09 12:23:58 -0300

    Ajusta nome curto do PWA

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.100';
-const APP_BUILD_DATE = '2026-07-09 12:23:04';
+const APP_VERSION = '1.0.101';
+const APP_BUILD_DATE = '2026-07-09 12:23:58';
 
 
 
