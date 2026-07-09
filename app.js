381af5119dbb1383bcf5b4d69b9623953ebe153d
Edney Gemini <edneypugliese.dev@gmail.com>
2026-07-09 12:16:38 -0300

    Ajusta card de pagamentos semanais

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.98';
-const APP_BUILD_DATE = '2026-07-09 08:19:52';
+const APP_VERSION = '1.0.99';
+const APP_BUILD_DATE = '2026-07-09 12:16:39';
 
 
 
