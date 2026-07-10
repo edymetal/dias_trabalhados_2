2bc589833b69edf320560c305e2b2d74315ff3f7
Edney Gemini <edneypugliese.dev@gmail.com>
2026-07-10 14:48:37 -0300

    Altera cor do valor recebido

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/1
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Versão da aplicação (gerenciada automaticamente pelo Git Hook)
-const APP_VERSION = '1.0.110';
-const APP_BUILD_DATE = '2026-07-10 14:44:11';
+const APP_VERSION = '1.0.111';
+const APP_BUILD_DATE = '2026-07-10 14:48:38';
 
 
 
